bb019144f309b74196e0a5d05dfada9fd2d79e5b
first_name last_name <email address>
2013-06-19 21:28:03 -0400

    added throwing on missing non-optional attributes

--- template.ts
+++ template.ts
@@ -1,5 +1,5 @@
-
 <% models.forEach(function(model){ %>
+
 class _<%= model.name %> extends Backbone.Model {
   <% model.members.forEach(function(member){ %>
 
@@ -14,11 +14,17 @@ class _<%= model.name %> extends Backbone.Model {
   static fromJSON(json) : <%= model.name %> {
     var attributes = {};
     <% model.members.forEach(function(member){ %>
-      <% if (member.optional) { %> if ("<%= member.name %>" in json) <% } %>
-      <% if (_.contains(['string', 'bool', 'number'], member.type)) { %>
-        attributes["<%= member.name %>"] = json["<%= member.name %>"];
-      <% } else { %>
-        attributes["<%= member.name %>"] = <%= member.type %>.fromJSON(json["<%= member.name %>"]);
+      if ("<%= member.name %>" in json) {
+        <% if (_.contains(['string', 'bool', 'number'], member.type)) { %>
+          attributes["<%= member.name %>"] = json["<%= member.name %>"];
+        <% } else { %>
+          attributes["<%= member.name %>"] = <%= member.type %>.fromJSON(json["<%= member.name %>"]);
+        <% } %>
+      }
+      <% if (!member.optional) { %>
+      else {
+        throw "JSON failed validation";
+      }
       <% } %>
     <% }); %>
     return new <%= model.name %>(attributes);
