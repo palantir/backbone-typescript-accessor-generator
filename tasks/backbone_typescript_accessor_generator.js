56a884d4d847238d104257370e5b802278804ba3
first_name last_name <email address>
2013-06-20 19:15:19 -0400

    added grunt task support

--- tasks/backbone_typescript_accessor_generator.js
+++ tasks/backbone_typescript_accessor_generator.js
@@ -8,13 +8,14 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+var modelmaker = require('../lib/modelmaker');
 
+module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
+  var description = 'Generates backbone models with typed subclasses for useing Backbone.js with TypeScript and its type system';
+  grunt.registerMultiTask('backbone_typescript_accessor_generator', description, function() {
 
-  grunt.registerMultiTask('backbone_typescript_accessor_generator', 'Your task description goes here.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       punctuation: '.',
       separator: ', '
@@ -22,25 +23,7 @@ module.exports = function(grunt) {
 
     // Iterate over all specified file groups.
     this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
+      modelmaker.generate_typescript(f.src[0], f.dest);
 
       // Print a success message.
       grunt.log.writeln('File "' + f.dest + '" created.');
