b9dff3efd0ac0c3d8bba7d57c87e61d8d4e12a94
first_name last_name <email address>
2013-06-20 13:56:56 -0400

    switched models from json to yaml

--- modelmaker.js
+++ modelmaker.js
@@ -1,12 +1,17 @@
 
 var fs = require('fs'),
-    us = require('underscore');
+    us = require('underscore'),
+  yaml = require('js-yaml');
 
-var models_location = process.argv[2] || 'models',
-    models_json_path = models_location+'.json',
-    models = JSON.parse(fs.readFileSync(models_json_path)),
-    out_path = '_'+models_location+'.ts';
+if (!process.argv[2]) {
+  console.log('requires argument');
+  process.exit();
+}
 
+var models_location = process.argv[2],
+    models_json_path = models_location+'.models.yml',
+    models = yaml.safeLoad(fs.readFileSync(models_json_path, encoding='utf-8')),
+    out_path = models_location+'.models.ts';
 
 function mapHash(hash, mapper) {
   return Object.keys(hash).map(function(key){
@@ -25,8 +30,8 @@ function checkAndRemove(arr, val) {
 var normalized_models = mapHash(models, function(model_name, members){
   return {
     name: model_name,
-    members: mapHash(members, function(member_name, member_opts){
-      if (!us.isArray(member_opts)) member_opts = [member_opts];
+    members: mapHash(members, function(member_name, member_def){
+      var member_opts = member_def.split(' ');
       return {
         name: member_name,
         readonly: checkAndRemove(member_opts, 'readonly'),
@@ -44,6 +49,7 @@ fs.writeFileSync(out_path, output);
 /*
 TODO:
 [ ] grunt plugin
+[ ] handle paths more reasonably
 [ ] add to npm
 [ ] throw on additional fields
 */
